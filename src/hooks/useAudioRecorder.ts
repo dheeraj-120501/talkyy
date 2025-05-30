62fc5b07cec91a09d53b8d2592b212b0737254ab
Nikhil Singh <nikhils@sahaj.ai>
2025-05-30 15:50:25 +0530

    Record audio in wav instead of webm

--- src/hooks/useAudioRecorder.ts
+++ src/hooks/useAudioRecorder.ts
@@ -1,15 +1,28 @@
 import { useState, useRef } from "react";
+import {
+  MediaRecorder,
+  type IMediaRecorder,
+  register,
+} from "extendable-media-recorder";
+import { connect } from "extendable-media-recorder-wav-encoder";
 
 export const useAudioRecorder = (
-  onRecordingComplete: (recording: Blob) => void
+  onRecordingComplete: (recording: Blob) => void,
 ) => {
   const [isRecording, setIsRecording] = useState(false);
-  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const mediaRecorderRef = useRef<IMediaRecorder | null>(null);
+  const wavMessagePortRef = useRef<MessagePort | null>(null);
 
   const startRecording = async () => {
     try {
+      if (wavMessagePortRef.current === null) {
+        wavMessagePortRef.current = await connect();
+        await register(wavMessagePortRef.current);
+      }
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      const recorder = new MediaRecorder(stream);
+      const recorder = new MediaRecorder(stream, {
+        mimeType: "audio/wav",
+      });
 
       const chunks: BlobPart[] = [];
       recorder.ondataavailable = (event) => {
@@ -21,7 +34,7 @@ export const useAudioRecorder = (
       recorder.onstop = () => {
         stream.getTracks().forEach((track) => track.stop());
 
-        const blob = new Blob(chunks, { type: "audio/webm" });
+        const blob = new Blob(chunks, { type: recorder.mimeType });
         onRecordingComplete(blob);
         mediaRecorderRef.current = null;
       };
